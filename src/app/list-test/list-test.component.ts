1315fa2e30846248b745ff7cac46670560bebcac
HaninMustafa <haninmustafa13@gmail.com>
2016-11-14 16:01:48 -0800

    first day form

---
+++ src/app/list-test/list-test.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+class Apple{
+  constructor(
+
+  public name: string,
+  public color: string
+  ){}
+
+}
+
+@Component({
+  selector: 'app-list-test',
+  templateUrl: './list-test.component.html',
+  styleUrls: ['./list-test.component.css']
+})
+
+export class ListTestComponent implements OnInit {
+
+appleList:[Apple];
+apple: Apple;
+
+constructor(){
+this.apple={name:'',color:''};
+this.appleList =[
+
+   {name:'l1', color:'l2'},
+   {name:'l3', color:'l4'},
+   {name:'l5', color:'l6'}
+];
+}
+
+addApple(){
+  this.appleList.push(this.apple);
+  this.apple = {name: '',color: ''};
+}
+enderAppleForm
+  ngOnInit() {
+  }
+
+}
