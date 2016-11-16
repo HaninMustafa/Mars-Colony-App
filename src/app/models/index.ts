db9038124fae8cad49afb616dc6365bea234f4a3
HaninMustafa <haninmustafa13@gmail.com>
2016-11-16 10:59:37 -0800

    fix my register form

--- src/app/models/index.ts
+++ src/app/models/index.ts
@@ -1,35 +1,42 @@
 export class Encounter {
     constructor(
-        public id : number,
+        public id: number,
         public date: string,
-        public colonist_id: number,
-        public atype : string,
-        public action : string,
+        public Colonist_id: number,
+        public atype: string,
+        public action: string
+
     ) {}
 }
 
-export class Colonist {
+export class NewColonist {
     constructor(
-        public name : string,
-        public job : Job,
-        public id : number,
-        public age : number,
-    ){}
+        public name: string,
+        public age: number,
+        public job_id: string
+
+    ) {}
+}
+interface Colonist {
+     name: string,
+     job: Job,
+     id:  number,
+     age: number
 }
 
 export class Job {
     constructor(
-        public name : string,
-        public id : number,
-        public description : string,
-    ){}
+        public name: string,
+        public id: number,
+        public description: string
+    ) {}
 }
 
-export class Alliens {
+export class Alien {
     constructor(
-        public type : string,
-        public submitted_by : string,
-        public id : number,
-        public description : string,
-    ){}
+        public type: string,
+        public submitted_by: string,
+        public id: number,
+        public description: string
+    ) {}
 }
